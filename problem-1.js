8c8cdddf56acc155cba82ea57b5d67d58fc3cd82
topu9872-cpu <topu9872@gmail.com>
2026-02-07 06:00:24 +0600

    commit

--- problem-1.js
+++ problem-1.js
@@ -1,12 +1,13 @@
 function newPrice(currentPrice , discount ) {
     if(typeof currentPrice !=='number' || typeof discount !== 'number'|| discount <=0 ||discount >=100){
         return 'Invalid'}
+    
       let discountPrice = (currentPrice*discount)/100;
       let payAmount = (discountPrice-discount);
-        return payAmount;
+        return payAmount.toFixed(3);
 
       
       
       }
 
-console.log(newPrice(10000, -190))
+console.log(newPrice(10000, 9.3))
